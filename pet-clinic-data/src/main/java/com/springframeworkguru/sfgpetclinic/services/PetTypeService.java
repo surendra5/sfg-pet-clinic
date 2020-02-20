fcf9e77f3c1037467c922713372804925c501821
surmeena <surendrameena5@gmail.com>
2020-02-20 20:55:29 +0530

    PetTypeMapService field to owner

---
+++ pet-clinic-data/src/main/java/com/springframeworkguru/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframeworkguru.sfgpetclinic.services;
+
+import com.springframeworkguru.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CRUDService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframeworkguru/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package com.springframeworkguru.sfgpetclinic.services.map;
+
+import com.springframeworkguru.sfgpetclinic.model.PetType;
+import com.springframeworkguru.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+}
